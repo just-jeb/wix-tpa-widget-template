8474bdd11eef59319c5e2422c77b70f752efe6fd
JeB <work@justjeb.com>
2022-10-06 16:08:13 +0300

    feat: WSR look and feel

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,47 @@
 import type React from 'react';
-import { st, classes } from './app.st.css';
-import { Header } from './header';
-import { Card } from 'wix-style-react';
-
+import { Avatar, Box, Card, ListItemSelect, TextButton, ThemeProvider } from 'wix-style-react';
+import { theme } from 'wix-style-react/themes/businessDashboard';
+import { Add } from 'wix-ui-icons-common';
 export interface AppProps {
   className?: string;
 }
 
-export const App: React.FC<AppProps> = ({ className }) => {
+export const App: React.FC<AppProps> = () => {
   return (
-    <Card>
-      <Card.Header title="Hello WSR" />
-      <Card.Content>
-        <main className={st(classes.root, className)}>
-          <Header className={classes.header} />
-        </main>
-      </Card.Content>
-    </Card>
+    <ThemeProvider theme={theme({ active: true })}>
+      <Card>
+        <Card.Header title="Hello TPA" suffix={ <TextButton size={'tiny'} weight='normal' prefixIcon={<Add/>}>Add New</TextButton>}/>
+        <Box paddingTop={'12px'} direction='vertical'>
+          <ListItemSelect
+            title="I am a WSR list Item"
+            prefix={<Avatar size="size30" />}
+            subtitle="Called ListItemSelect"
+            suffix="11 Sep 1988"
+            size='medium'
+          />
+          <Card.Divider />
+          <ListItemSelect
+            title="I am brought from remote"
+            prefix={<Avatar size="size30" />}
+            subtitle="A simulated on of course"
+            suffix="16 May 2016"
+            size='medium'
+          />
+          <Card.Divider />
+          <ListItemSelect
+            title="I am clickable"
+            prefix={<Avatar size="size30" />}
+            subtitle="The CTAs are clickable too"
+            suffix="11 Dec 2019"
+            size='medium'
+
+          />
+          <Card.Divider />
+        </Box>
+        <Box padding={'16px 24px'}>
+          <TextButton size={'tiny'} weight='normal'>See All(10)</TextButton>
+        </Box>
+      </Card>
+    </ThemeProvider>
   );
 };
