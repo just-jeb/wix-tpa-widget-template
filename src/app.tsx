f20f56f11386e39bf5f22d33f3ef5aad4c5c2e11
JeB <work@justjeb.com>
2022-10-06 14:35:44 +0300

    chore: add WSR

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,21 @@
 import type React from 'react';
 import { st, classes } from './app.st.css';
 import { Header } from './header';
+import { Card } from 'wix-style-react';
 
 export interface AppProps {
-    className?: string;
+  className?: string;
 }
 
 export const App: React.FC<AppProps> = ({ className }) => {
-    return (
+  return (
+    <Card>
+      <Card.Header title="Hello WSR" />
+      <Card.Content>
         <main className={st(classes.root, className)}>
-            <Header className={classes.header} />
+          <Header className={classes.header} />
         </main>
-    );
+      </Card.Content>
+    </Card>
+  );
 };
