52a39859836ac62914de7551ec4ea1268814079b
JeB <work@justjeb.com>
2022-10-06 16:17:21 +0300

    chore: minor fixes

--- src/app.tsx
+++ src/app.tsx
@@ -1,42 +1,46 @@
+import type { FC } from 'react';
 import type React from 'react';
-import { Avatar, Box, Card, ListItemSelect, TextButton, ThemeProvider } from 'wix-style-react';
+import { Avatar, Box, Card, ListItemSelect, ListItemSelectProps, TextButton, ThemeProvider } from 'wix-style-react';
 import { theme } from 'wix-style-react/themes/businessDashboard';
 import { Add } from 'wix-ui-icons-common';
 export interface AppProps {
   className?: string;
 }
 
+const ListItem: FC<ListItemSelectProps> = ({ title, subtitle, suffix }) => (
+  <><ListItemSelect
+    title={title}
+    prefix={<Box paddingLeft={'12px'}><Avatar size="size30" /></Box>}
+    subtitle={subtitle}
+    suffix={<Box paddingRight={'12px'}>{suffix}</Box>}
+    size='medium'
+  />
+    <Box padding={'0 24px'}>
+      <Card.Divider />
+    </Box></>
+)
+
 export const App: React.FC<AppProps> = () => {
   return (
     <ThemeProvider theme={theme({ active: true })}>
       <Card>
-        <Card.Header title="Hello TPA" suffix={ <TextButton size={'tiny'} weight='normal' prefixIcon={<Add/>}>Add New</TextButton>}/>
+        <Card.Header title="Hello TPA" suffix={<TextButton size={'tiny'} weight='normal' prefixIcon={<Add />}>Add New</TextButton>} />
         <Box paddingTop={'12px'} direction='vertical'>
-          <ListItemSelect
+          <ListItem
             title="I am a WSR list Item"
-            prefix={<Avatar size="size30" />}
             subtitle="Called ListItemSelect"
-            suffix="11 Sep 1988"
-            size='medium'
+            suffix="21 Sep 1988"
           />
-          <Card.Divider />
-          <ListItemSelect
+          <ListItem
             title="I am brought from remote"
-            prefix={<Avatar size="size30" />}
-            subtitle="A simulated on of course"
+            subtitle='A simulated one of course'
             suffix="16 May 2016"
-            size='medium'
           />
-          <Card.Divider />
-          <ListItemSelect
+          <ListItem
             title="I am clickable"
-            prefix={<Avatar size="size30" />}
             subtitle="The CTAs are clickable too"
             suffix="11 Dec 2019"
-            size='medium'
-
           />
-          <Card.Divider />
         </Box>
         <Box padding={'16px 24px'}>
           <TextButton size={'tiny'} weight='normal'>See All(10)</TextButton>
